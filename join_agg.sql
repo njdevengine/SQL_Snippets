170c8129c816fee5db453ac6c71ba845ddf9839d
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-09-11 13:51:56 -0400

    Update join_agg.sql

--- join_agg.sql
+++ join_agg.sql
@@ -3,3 +3,14 @@ FROM cities
 JOIN users
   ON cities.id = users.city_id
 GROUP BY cities.cityname
+--
+SELECT
+    cities.cityname,
+    SUM(users.age) AS sum,
+    COUNT(users.id) AS count,
+    SUM(users.age) / COUNT(users.id) AS average
+FROM cities
+LEFT JOIN users
+  ON cities.id = users.city_id
+GROUP BY cities.cityname
+
