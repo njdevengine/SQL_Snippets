d9b6bbd999bc07cdc840e616c68334318da2560b
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-09-14 00:23:57 -0400

    Update sakila_queries.sql

--- sakila_queries.sql
+++ sakila_queries.sql
@@ -1,1 +1,4 @@
 select * from actor where first_name like "%e%" order by first_name desc limit 10;
+
+-- get actors that were in an action film
+select * from actor where exists(select * from actor_info where actor.actor_id = actor_info.actor_id and actor_info.film_info like "%Action%");
