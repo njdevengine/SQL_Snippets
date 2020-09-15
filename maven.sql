d2fec17e916c627bfacb108a10f820cf9e80b212
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-09-15 00:27:39 -0400

    Create maven.sql

---
+++ maven.sql
@@ -0,0 +1,7 @@
+-- get count of unique sessions by traffic source
+select utm_content, count(distinct website_session_id) as sessions
+from website_sessions
+where user_id between 1000 and 2000
+group by
+utm_content;
+
