6f84ec9f7b42eea2ef1bc5049c066a84cf6c3203
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-09-15 02:00:56 -0400

    Update maven.sql

--- maven.sql
+++ maven.sql
@@ -86,3 +86,17 @@ left join orders on orders.website_session_id = website_sessions.website_session
 where website_sessions.utm_source = "gsearch" and website_sessions.utm_campaign = "nonbrand"
 ;
 
+-- same with date filter
+
+select 
+	count(distinct website_sessions.website_session_id) as sessions,
+	count(distinct orders.website_session_id) as orders,
+	count(distinct orders.website_session_id)/count(distinct website_sessions.website_session_id) as conv_rate
+from website_sessions
+left join orders on orders.website_session_id = website_sessions.website_session_id
+where website_sessions.utm_source = "gsearch" and website_sessions.utm_campaign = "nonbrand"
+AND website_sessions.created_at < '2012-04-14'
+;
+
+--
+
