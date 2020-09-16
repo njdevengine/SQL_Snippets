9e984679d25f5d6cb8eb84045592257f5cd6efec
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-09-15 22:18:43 -0400

    Update maven.sql

--- maven.sql
+++ maven.sql
@@ -145,3 +145,17 @@ and website_sessions.utm_source = 'gsearch'
 and website_sessions.utm_campaign = 'nonbrand'
 group by 1
 ;
+
+-- get breakdown of sessions by week with total desktop and mobile sessions weekly
+	 
+select
+min(date(created_at)),
+-- count(distinct website_session_id) as sessions,
+count(distinct case when device_type = 'desktop' then website_session_id else null end) as desktop_session,
+count(distinct case when device_type = 'mobile' then website_session_id else null end) as mobile_session
+from website_sessions
+where created_at <= '2012-06-09'
+and created_at >= '2012-04-15'
+and utm_source = 'gsearch'
+and utm_campaign = 'nonbrand'
+group by week(created_at);
