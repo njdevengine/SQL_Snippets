49c070def7cf3d7b37bc31551537c8e21b888c9e
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-09-16 21:58:13 -0400

    Update maven.sql

--- maven.sql
+++ maven.sql
@@ -188,3 +188,15 @@ group by 1
 order by first
 desc
 ;
+
+-- get a summary of sessions per page
+
+select
+pageview_url,
+count(distinct website_pageview_id) as sessions
+from website_pageviews
+where created_at < '2012-06-09'
+group by pageview_url
+order by sessions
+desc
+;
